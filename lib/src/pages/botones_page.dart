7d4c2d2be82757c554d42b2bd092b6b65cdbf248
FlutLab.io <>
2021-05-18 20:18:06 +0000

    Initial commit

---
+++ lib/src/pages/botones_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class BotonesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Botones'),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  print('Click');
+                },
+                color: Colors.yellow,
+                child: Text(
+                  'Soy un Boton',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  print('Click');
+                },
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                color: Colors.purple[500],
+                child: Text(
+                  'Soy un Boton',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.settings),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.orange[200],
+                child: Text('Click'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
